da93840427d9af2e82061e15954105fb2d7db62b
Tahsin Islam Srijon <88778379+TISnoob@users.noreply.github.com>
2024-09-13 15:29:12 +0600

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2024 @TISnoob
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_WALPAD8G_V2.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_WALPAD8G_V2-user \
+    lineage_WALPAD8G_V2-userdebug \
+    lineage_WALPAD8G_V2-eng
